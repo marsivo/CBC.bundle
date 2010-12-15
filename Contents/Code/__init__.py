45fc476b912efc7bba828a016dae46ada36f4964
sander1 <sanderspies+plexapp@gmail.com>
2010-12-15 03:10:49 +0100

    v2 update

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,4 +1,3 @@
-import urllib
 import time
 
 ####################################################################################################
@@ -7,13 +6,11 @@ import time
 #Written by mysciencefriend - look me up on the plexapp.com forums for help
 #Use at your own risk, etc. etc.
 
-
-VIDEO_PREFIX = "/video/cbc"
-
+VIDEO_PREFIX = '/video/cbc'
 NAME = 'CBC'
 
-ART           = 'art-default.jpg'
-ICON          = 'icon-default.png'
+ART  = 'art-default.jpg'
+ICON = 'icon-default.png'
 
 CATEGORY_LIST = {'News'		:	'1221258968',
 				'Sports'	:	'1237510081',
@@ -38,48 +35,48 @@ SHOWEXCEPTIONS = {'The Rick Mercer Report'	:	'Rick Mercer Report',
 				'Hockey Night in Canada'	:	'HNIC',
 				'Q on bold'					:	'Q'
 				}
-				
-SHOWEXCEPTIONS_REV = {'Rick Mercer Report'		:	'The Rick Mercer Report',
-					'Ron James Show'			:	'The Ron James Show',
-					'HNIC'						:	'Hockey Night in Canada',
-					'Q'							:	'Q on bold'
-					}
 
+SHOWEXCEPTIONS_REV = {'Rick Mercer Report'	:	'The Rick Mercer Report',
+					'Ron James Show'		:	'The Ron James Show',
+					'HNIC'					:	'Hockey Night in Canada',
+					'Q'						:	'Q on bold'
+					}
 
 SHOWSLIST = 'http://cbc.feeds.theplatform.com/ps/JSON/PortalService/2.2/getCategoryList?PID=_DyE_l_gC9yXF9BvDQ4XNfcCVLS4PQij&field=ID&field=title&field=parentID&field=description&customField=MaxClips&customField=ClipType&query=ParentIDs|1221254309'
 LOCALLIST = 'http://cbc.feeds.theplatform.com/ps/JSON/PortalService/2.2/getCategoryList?PID=_DyE_l_gC9yXF9BvDQ4XNfcCVLS4PQij&field=ID&field=title&field=parentID&field=description&customField=MaxClips&customField=ClipType&query=ParentIDs|1244502941'
 CLIPSURL = 'http://cbc.feeds.theplatform.com/ps/JSON/PortalService/2.2/getReleaseList?PID=_DyE_l_gC9yXF9BvDQ4XNfcCVLS4PQij&query=CategoryIDs|%s&sortField=%s&sortDescending=true&endIndex=500'
 SHOWSURL = 'http://cbc.feeds.theplatform.com/ps/JSON/PortalService/2.2/getReleaseList?PID=_DyE_l_gC9yXF9BvDQ4XNfcCVLS4PQij&field=title&field=PID&field=ID&field=description&field=categoryIDs&field=thumbnailURL&field=URL&field=added&field=airdate&field=expirationDate&field=length&field=Keywords&query=%s&sortField=airdate&sortDescending=true&startIndex=1&endIndex=%s'
 WATCHURL = 'http://www.cbc.ca/video/#/%s/ID='
+
 ####################################################################################################
 
 def Start():
+    Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, NAME, ICON, ART)
+    Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
 
-    Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, 'CBC', ICON, ART)
-    Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
-    Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
     MediaContainer.art = R(ART)
     MediaContainer.title1 = NAME
+    MediaContainer.viewGroup = 'InfoList'
     DirectoryItem.thumb = R(ICON)
 
-  
-######################################
+    HTTP.CacheTime = 1800
+
+####################################################################################################
 
 def VideoMainMenu():
-
-	dir = MediaContainer(viewGroup="InfoList",title='CBC')
+	dir = MediaContainer()
 
 	dir.Append(Function(DirectoryItem(NewsMenu,L('NewsMenu'),subtitle="",summary=L('NewsSummary'),thumb=R(ICON),art=R(ART))))
 	dir.Append(Function(DirectoryItem(ShowsMenu,L('ShowsMenu'),subtitle="",summary=L('ShowsSummary'),thumb=R(ICON),art=R(ART),),level=1, title2=L('ShowsMenu')))
 	dir.Append(Function(DirectoryItem(SportsMenu,L('SportsMenu'),subtitle="",summary=L('SportsSummary'),thumb=R(ICON),art=R(ART))))
-	HTTP.SetCacheTime(1800)
-   
+
 	return dir
 
-###########################
+####################################################################################################
+
 def NewsMenu(sender):
 	url = 'http://www.cbc.ca/thenational/watch/'
-	dir = MediaContainer(viewGroup="InfoList", title2="News")
+	dir = MediaContainer(title2="News")
 	dir.Append(WebVideoItem(url,L('TheNationalTitle'), subtitle=L('TheNationalSubtitle'), summary=L('TheNationalSummary'), duration="3600000", thumb=R('the_national_program.jpg')))
 	dir.Append(Function(DirectoryItem(GetRSS,L('OnDemandTitle'),subtitle="",summary=L('OnDemandSummary'),thumb=R('bg-yourNational.jpg'),art=R(ART),),show="NoD", title2=L('OnDemandTitle')))
 	dir.Append(Function(DirectoryItem(GetRSS,L('AtIssueTitle'),subtitle="",summary=L('AtIssueSummary'),thumb=R('atissue.png'),art=R(ART),),show="AtIssue", title2=L('AtIssueTitle')))
@@ -88,18 +85,20 @@ def NewsMenu(sender):
 	dir.Append(Function(DirectoryItem(GetClips,title=L('NewsPopular'),summary=L('NewsPopularSummary'),thumb=R(ICON),art=R(ART),),category="News", arrange="requestCount", title2=L('MostWatched')))
 	dir.Append(Function(DirectoryItem(LocalMenu,L('LocalMenu'),subtitle="",summary=L('NewsLocalSummary'),thumb=R(ICON),art=R(ART),),title2=L('ShowsMenu')))
 	return dir
-###################################
+####################################################################################################
+
 def SportsMenu(sender):
-	dir = MediaContainer(viewGroup="InfoList", title2="Sports")
+	dir = MediaContainer(title2="Sports")
 	dir.Append(Function(DirectoryItem(ShowsMenu,L('LiveMenu'),subtitle="",summary=L('ShowsSummary'),thumb=R(ICON),art=R(ART),),level=2, title2=L('LiveMenu'),clipType="",category='Sports',showName='Live_Streaming'))
 	dir.Append(Function(DirectoryItem(GetClips,L('SportsRecent'),subtitle="",summary=L('SportsRecentSummary'),thumb=R(ICON),art=R(ART),),category="Sports", arrange="airdate", title2=L('Latest')))
 	dir.Append(Function(DirectoryItem(GetClips,title=L('SportsPopular'),summary=L('SportsPopularSummary'),thumb=R(ICON),art=R(ART),),category="Sports", arrange="requestCount", title2=L('MostWatched')))
 	dir.Append(Function(DirectoryItem(ShowsMenu,L('HNICMenu'),subtitle="",summary=L('HNICSummary'),thumb=R(ICON),art=R(ART),),level=2, title2=L('HNICMenu'),clipType="",category='Shows',showName='HNIC', maxClips='500'))
 	return dir
 
-#############################
+####################################################################################################
+
 def ShowsMenu(sender, level, title2, showName="",category='Shows', currentPage=0, clipType="",maxClips="",subtitle=""):
-	dir = MediaContainer(viewGroup="InfoList", title2=title2)
+	dir = MediaContainer(title2=title2)
 	if level == 1:
 		cbcJson = JSON.ObjectFromURL(SHOWSLIST, cacheTime=600)['items']
 		level=int(level)
@@ -112,11 +111,11 @@ def ShowsMenu(sender, level, title2, showName="",category='Shows', currentPage=0
 			else:
 				maxClips = 500	
 			if show['title'] in SHOWEXCEPTIONS:
-				showName = urllib.quote(SHOWEXCEPTIONS[show['title']])
+				showName = String.Quote(SHOWEXCEPTIONS[show['title']], usePlus=False)
 			elif show['title'] == 'Holiday Guide' or show['title'] == 'More Shows':
 				continue
 			else:
-				showName = urllib.quote(show['title'])
+				showName = String.Quote(show['title'], usePlus=False)
 			dir.Append(Function(DirectoryItem(ShowsMenu,title=title,summary=summary,thumb=R(ICON),art=R(ART),), level=level+1, title2=title, showName=showName,clipType=clipType, maxClips=maxClips))
 	elif level == 2:
 		epUrl = GetEpisodesUrl(showName, clipType, maxClips)
@@ -135,8 +134,8 @@ def ShowsMenu(sender, level, title2, showName="",category='Shows', currentPage=0
 			pageEnd = int(pageStart + 20)
 		cbcJsonCurrPage = cbcJson[pageStart:pageEnd]
 		for show in cbcJsonCurrPage:
-			if urllib.unquote(showName) in SHOWEXCEPTIONS_REV:
-				showName = urllib.quote(SHOWEXCEPTIONS_REV[urllib.unquote(showName)])
+			if String.Unquote(showName, usePlus=False) in SHOWEXCEPTIONS_REV:
+				showName = String.Quote(SHOWEXCEPTIONS_REV[String.Unquote(showName, usePlus=False)], usePlus=False)
 			title = show['title']
 			url = WATCHURL % (category + '/' + showName.replace('%20','_')) + str(show['ID'])
 			length = show['length']
@@ -154,12 +153,13 @@ def ShowsMenu(sender, level, title2, showName="",category='Shows', currentPage=0
 			thumb = show['thumbnailURL']
 			dir.Append(WebVideoItem(url, title, summary=summary, thumb=thumb, duration=length, subtitle=subtitle))
 		if currentPage < pages:
-			if urllib.unquote(showName) in SHOWEXCEPTIONS:
-				showName = urllib.quote(SHOWEXCEPTIONS[urllib.unquote(showName)])
+			if String.Unquote(showName, usePlus=False) in SHOWEXCEPTIONS:
+				showName = String.Quote(SHOWEXCEPTIONS[String.Unquote(showName, usePlus=False)], usePlus=False)
 			dir.Append(Function(DirectoryItem(ShowsMenu,title=L('NextPage'),summary=L('NextPageSummary'),thumb=R(ICON),art=R(ART),),level=2, title2=title2, showName=showName, currentPage=currentPage+1,clipType=clipType, maxClips=maxClips))
 	return dir
 
-###########################
+####################################################################################################
+
 def GetClips(sender, category, arrange, title2, currentPage=0):
 	cbcData = JSON.ObjectFromURL(CLIPSURL % (CATEGORY_LIST[category], arrange), cacheTime=600)
 	cbcJson = cbcData['items']
@@ -175,7 +175,7 @@ def GetClips(sender, category, arrange, title2, currentPage=0):
 	else:
 		pageEnd = int(pageStart + 20)
 	cbcJsonCurrPage = cbcJson[pageStart:pageEnd]
-	dir = MediaContainer(viewGroup="InfoList",title=category,title2=title2)
+	dir = MediaContainer(title1=category,title2=title2)
 	for clip in cbcJsonCurrPage:
 		title = clip['title']
 		url = WATCHURL % category + str(clip['ID'])
@@ -187,28 +187,7 @@ def GetClips(sender, category, arrange, title2, currentPage=0):
 		dir.Append(Function(DirectoryItem(GetClips,title=L('NextPage'),summary="More %s Clips" % category,thumb=R(ICON),art=R(ART),),category=category,arrange=arrange, title2=title2, currentPage=currentPage+1))
 	return dir
 
-
-########################################
-#def GetRSS(sender, title2, show=""):
-#	if show == "NoD":
-#		dataURL = "http://www.cbc.ca/podcasting/includes/thenational-video-podcast.xml"
-#		thumb = R('bg-yourNational.jpg')
-#	elif show == "AtIssue":
-#		dataURL = "http://www.cbc.ca/mediafeeds/rss/cbc/atissue-video-podcast.xml"
-#		thumb = R('atissue.png')
-#	elif show == "RexMurphy":
-#		dataURL = "http://www.cbc.ca/mediafeeds/rss/cbc/rexmurphy-video-podcast.xml"
-#		thumb = R('rexmurphy.jpg')
-#	cbcRSS = RSS.FeedFromURL(dataURL, cacheTime=600)['entries']
-#	dir = MediaContainer(viewGroup="InfoList", title2=title2)
-#	for clip in cbcRSS:
-#		title = clip['title']
-#		summary = clip['summary']
-#		url = clip['link']
-#		length = int(clip['itunes_duration']) * 1000
-#		dir.Append(VideoItem(url, title, thumb=thumb, summary=summary, duration=length))
-#	return dir
-#
+####################################################################################################
 
 def GetRSS(sender, title2, show=""):
         if show == "NoD":
@@ -221,7 +200,7 @@ def GetRSS(sender, title2, show=""):
                 dataURL = "http://www.cbc.ca/mediafeeds/rss/cbc/rexmurphy-video-podcast.xml"
                 thumb = R('rexmurphy.jpg')
         cbcRSS = XML.ElementFromURL(dataURL, cacheTime=600).xpath('/rss/channel/item')
-        dir = MediaContainer(viewGroup="InfoList", title2=title2)
+        dir = MediaContainer(title2=title2)
         for clip in cbcRSS:
                 title = clip.xpath('./title')[0].text
                 summary = clip.xpath('./itunes:summary', namespaces={'itunes':'http://www.itunes.com/dtds/podcast-1.0.dtd'})[0].text
@@ -231,7 +210,8 @@ def GetRSS(sender, title2, show=""):
                 dir.Append(VideoItem(url, title, thumb=thumb, summary=summary, duration=length))
         return dir
 
-###########################################
+####################################################################################################
+
 def GetEpisodesUrl(showName, clipType, maxClips='500'):
 	query = 'ContentCustomText|Show|' + showName
 	if showName == 'HNIC':
@@ -241,15 +221,14 @@ def GetEpisodesUrl(showName, clipType, maxClips='500'):
 		url = 'http://cbc.feeds.theplatform.com/ps/JSON/PortalService/2.2/getReleaseList?PID=_DyE_l_gC9yXF9BvDQ4XNfcCVLS4PQij&field=title&field=PID&field=ID&field=description&field=categoryIDs&field=thumbnailURL&field=URL&field=added&field=airdate&field=expirationDate&field=length&field=Keywords&contentCustomField=backgroundImage&contentCustomField=show&contentCustomField=relatedURL1&contentCustomField=relatedURL2&contentCustomField=relatedURL3&contentCustomField=sport&contentCustomField=seasonNumber&contentCustomField=clipType&contentCustomField=segment&contentCustomField=event&contentCustomField=adCategory&contentCustomField=LiveOnDemand&contentCustomField=AudioVideo&contentCustomField=EpisodeNumber&contentCustomField=RelatedClips&contentCustomField=Genre&contentCustomField=CommentsEnabled&contentCustomField=CommentsExpirationDate&query=CategoryIDs|1248955900&sortField=airdate&endIndex=500'
 	else:
 		if clipType == "Full Program":
-			query = query + '&query=ContentCustomText|ClipType|' + urllib.quote(clipType)
+			query = query + '&query=ContentCustomText|ClipType|' + String.Quote(clipType, usePlus=False)
 		url = SHOWSURL % (query, maxClips)
 	return url
 
-
-#############################################
+####################################################################################################
 
 def LocalMenu(sender, title2, showName="",category="", currentPage=0, clipType="",maxClips="",subtitle=""):
-	dir = MediaContainer(viewGroup="InfoList", title2=title2)
+	dir = MediaContainer(title2=title2)
 	cbcJson = JSON.ObjectFromURL(LOCALLIST, cacheTime=600)['items']
 	for show in cbcJson:
 		title = show['title']
@@ -259,6 +238,6 @@ def LocalMenu(sender, title2, showName="",category="", currentPage=0, clipType="
 			maxClips = show['customData'][0]['value']
 		else:
 			maxClips = 500	
-		showName = urllib.quote(show['title'])
+		showName = String.Quote(show['title'], usePlus=False)
 		dir.Append(Function(DirectoryItem(GetClips,title=title,subtitle="",summary=L('NewsLocalSummary'),thumb=R(ICON),art=R(ART),),category=title, arrange="airdate", title2=L('Latest')))
 	return dir
