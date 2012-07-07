59c290d962d97fd484fa80ae6c29a53d2b6163b1
Mike M <mike.d.meier@gmail.com>
2012-07-07 16:00:44 -0600

    clean up spaces vs tabs

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -33,7 +33,7 @@ def VideoMainMenu():
     
     oc.add(DirectoryObject(key=Callback(ShowCategories), title='Shows'))
     for category in CATEGORIES:
-	oc.add(DirectoryObject(key=Callback(NewsCategories, category=category), title=category))
+        oc.add(DirectoryObject(key=Callback(NewsCategories, category=category), title=category))
     oc.add(SearchDirectoryObject(identifier="com.plexapp.plugins.cbcnewsnetwork", title="Search", summary="Search CBC videos", prompt="Search for...",
         thumb=R(ICON), art=R(ART)))
     return oc
@@ -44,35 +44,35 @@ def ShowCategories():
     data = HTML.ElementFromURL(PLAYER_URL % 'Shows')
     categories = data.xpath('//div[@id="catnav"]//a[@class="haschildren"]')
     for category in categories:
-	title = category.text
-	cat_id = category.xpath('parent::li')[0].get('data-id')
-	oc.add(DirectoryObject(key=Callback(ShowsMenu, id=cat_id, title2=title), title=title))
+        title = category.text
+        cat_id = category.xpath('parent::li')[0].get('data-id')
+        oc.add(DirectoryObject(key=Callback(ShowsMenu, id=cat_id, title2=title), title=title))
     return oc
 
 ####################################################################################################
 def ShowsMenu(id, title1=None, title2=None):
     oc = ObjectContainer()
     if title1:
-	oc.title1=title1
+        oc.title1=title1
     if title2:
-	oc.title2=title2
+        oc.title2=title2
     
     data = JSON.ObjectFromURL(SHOWS_LIST % id)
     
     if data['listInfo']['itemCount'] == 0:
-	return VideoMenu(id, title1, title2)
+        return VideoMenu(id, title1, title2)
     else:
-	pass
+        pass
     
     for item in data['items']:
-	title = item['title']
-	id = item['ID']
-	oc.add(DirectoryObject(key=Callback(ShowsMenu, id=id, title1=title2, title2=title), title=title))
-	
+        title = item['title']
+        id = item['ID']
+        oc.add(DirectoryObject(key=Callback(ShowsMenu, id=id, title1=title2, title2=title), title=title))
+        
     oc.objects.sort(key = lambda obj: obj.title)
     if title2 == 'Shows':
-	oc.add(DirectoryObject(key=Callback(ShowsMenu, id=MORE_SHOWS, title2='More Shows'), title='More Shows'))
-	
+        oc.add(DirectoryObject(key=Callback(ShowsMenu, id=MORE_SHOWS, title2='More Shows'), title='More Shows'))
+        
     return oc
 
 ####################################################################################################
@@ -80,23 +80,23 @@ def NewsCategories(category):
     oc = ObjectContainer(title2=category)
     data = HTML.ElementFromURL(PLAYER_URL % String.Quote(category, usePlus=True))
     for item in data.xpath('//div[@id="catnav"]//a'):
-	url = item.get('href')
-	name = item.text
-	if name == 'Local News':
-	    url = url.replace('Local+News', 'Canada')
-	oc.add(DirectoryObject(key=Callback(NewsSortMenu, title1=category, title2=name, url=url), title=name))
+        url = item.get('href')
+        name = item.text
+        if name == 'Local News':
+            url = url.replace('Local+News', 'Canada')
+        oc.add(DirectoryObject(key=Callback(NewsSortMenu, title1=category, title2=name, url=url), title=name))
     
     if len(oc) == 0:
-	return ObjectContainer(header=L("Empty"), message=L('No content found.'))
+        return ObjectContainer(header=L("Empty"), message=L('No content found.'))
     else:
-	return oc
+        return oc
 
 ####################################################################################################
 def NewsSortMenu(title1, title2, url):
     oc = ObjectContainer(title1=title1, title2=title2)
     for option in ['Featured', 'Most Popular', 'Most Recent']:
-	oc.add(DirectoryObject(key=Callback(NewsMenu, title1=title1, title2=title2, url=url,
-	    sort_type=option.replace(' ', '')), title=option))
+        oc.add(DirectoryObject(key=Callback(NewsMenu, title1=title1, title2=title2, url=url,
+            sort_type=option.replace(' ', '')), title=option))
     return oc
 
 ####################################################################################################
@@ -104,81 +104,81 @@ def NewsMenu(title1, title2, url, sort_type=None, page=1, categories=[]):
     oc = ObjectContainer(title1=title1, title2=title2)
     data_url = BASE_URL + url + '?page=' + str(page)
     if sort_type:
-	data_url = data_url + '&sort=' + sort_type
+        data_url = data_url + '&sort=' + sort_type
     data = HTML.ElementFromURL(data_url)
     if page == 1:
-	subcategories = data.xpath('//div[@id="catnav"]//a')
-	new_categories = [subcategory.text for subcategory in subcategories]
-	for subcategory in subcategories:
-	    url = subcategory.get('href')
-	    name = subcategory.text
-	    if name not in categories:
-		oc.add(DirectoryObject(key=Callback(NewsMenu, title1=title2, title2=name, url=url, sort_type=sort_type, page=page, categories=new_categories), title=name))
-	    else:
-		pass
+        subcategories = data.xpath('//div[@id="catnav"]//a')
+        new_categories = [subcategory.text for subcategory in subcategories]
+        for subcategory in subcategories:
+            url = subcategory.get('href')
+            name = subcategory.text
+            if name not in categories:
+                oc.add(DirectoryObject(key=Callback(NewsMenu, title1=title2, title2=name, url=url, sort_type=sort_type, page=page, categories=new_categories), title=name))
+            else:
+                pass
     else:
-	pass
+        pass
     if title2 == 'Local News':
-	return oc
+        return oc
     
     for clip in data.xpath('//div[@class="clips"]//div[contains(@class, "clip col")]'):
-	clip_url = BASE_URL + clip.xpath('.//a')[0].get('href')
-	title = clip.xpath('.//img')[0].get('alt')
-	thumb = clip.xpath('.//img')[0].get('src')
-	try:
-	    date = Datetime.ParseDate(clip.xpath('.//span[@class="date"]')[0].text).date()
-	except:
-	    date=None
-	try:
-	    runtime = clip.xpath('.//span[@class="length"]')[0].text.split(':')
-	    if len(runtime) == 3:
-		duration = ((int(runtime[0])*60 + int(runtime[1]))*60 + int(runtime[2]))*1000
-	    elif len(runtime) == 2:
-	        duration = (int(runtime.split(':')[0])*60 + int(runtime.split(':')[1]))*1000
-	    else:
-	        duration = int(runtime[0])*1000
-	except:
-	    duration = None
-	try:
-	    summary = clip.xpath('.//span[@class="desc"]')[0].text
-	except:
-	    summary = ''
-	oc.add(VideoClipObject(url=clip_url, title=title, summary=summary, duration=duration, originally_available_at=date,
-	    thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
+        clip_url = BASE_URL + clip.xpath('.//a')[0].get('href')
+        title = clip.xpath('.//img')[0].get('alt')
+        thumb = clip.xpath('.//img')[0].get('src')
+        try:
+            date = Datetime.ParseDate(clip.xpath('.//span[@class="date"]')[0].text).date()
+        except:
+            date=None
+        try:
+            runtime = clip.xpath('.//span[@class="length"]')[0].text.split(':')
+            if len(runtime) == 3:
+                duration = ((int(runtime[0])*60 + int(runtime[1]))*60 + int(runtime[2]))*1000
+            elif len(runtime) == 2:
+                duration = (int(runtime.split(':')[0])*60 + int(runtime.split(':')[1]))*1000
+            else:
+                duration = int(runtime[0])*1000
+        except:
+            duration = None
+        try:
+            summary = clip.xpath('.//span[@class="desc"]')[0].text
+        except:
+            summary = ''
+        oc.add(VideoClipObject(url=clip_url, title=title, summary=summary, duration=duration, originally_available_at=date,
+            thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
     
     try:
-	total_pages = int(data.xpath('//span[@class="totalpages"]')[0].text)
+        total_pages = int(data.xpath('//span[@class="totalpages"]')[0].text)
     except:
-	total_pages = 1
+        total_pages = 1
     if total_pages > page:
-	page = page+1
-	oc.add(DirectoryObject(key=Callback(NewsMenu, title1=title2, title2="Page %s" % page, url=url, sort_type=sort_type, page=page), title='More'))
+        page = page+1
+        oc.add(DirectoryObject(key=Callback(NewsMenu, title1=title2, title2="Page %s" % page, url=url, sort_type=sort_type, page=page), title='More'))
     
     if len(oc) == 0:
-	return ObjectContainer(header=L("Empty"), message=L('No content found.'))
+        return ObjectContainer(header=L("Empty"), message=L('No content found.'))
     else:
-	return oc
+        return oc
     
 ####################################################################################################
 def VideoMenu(id, title1=None, title2=None):
     oc = ObjectContainer()
     if title1:
-	oc.title1=title1
+        oc.title1=title1
     if title2:
-	oc.title2=title2
+        oc.title2=title2
     data = JSON.ObjectFromURL(VIDEOS_LIST % id)
     for item in data['items']:
-	title = item['title']
-	summary = item['description']
-	duration = int(item['length'])
-	date = int(item['airdate'])/1000
-	date = Datetime.FromTimestamp(date).date()
-	thumbs = sorted(item['assets'], key = lambda thumb: int(thumb["height"]), reverse=True)
-	id = item['ID']
-	oc.add(VideoClipObject(url=PLAYER_URL % id, title=title, summary=summary, originally_available_at=date,
-	    thumb=Resource.ContentsOfURLWithFallback(url=[thumb['URL'] for thumb in thumbs], fallback=ICON)))
+        title = item['title']
+        summary = item['description']
+        duration = int(item['length'])
+        date = int(item['airdate'])/1000
+        date = Datetime.FromTimestamp(date).date()
+        thumbs = sorted(item['assets'], key = lambda thumb: int(thumb["height"]), reverse=True)
+        id = item['ID']
+        oc.add(VideoClipObject(url=PLAYER_URL % id, title=title, summary=summary, originally_available_at=date,
+            thumb=Resource.ContentsOfURLWithFallback(url=[thumb['URL'] for thumb in thumbs], fallback=ICON)))
     
     if len(oc) == 0:
-	return ObjectContainer(header=L("Empty"), message=L('No content found.'))
+        return ObjectContainer(header=L("Empty"), message=L('No content found.'))
     else:
-	return oc
+        return oc
