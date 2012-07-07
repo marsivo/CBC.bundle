25b62ce4cd644b4f9ce319c90578b6285817cb2c
Mike M <mike.d.meier@gmail.com>
2012-07-07 13:16:52 -0600

    major overhaul

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,9 +1,7 @@
-import time
-
 ####################################################################################################
-#Version 0.08
 #CBC.CA Video Plugin
-#Written by mysciencefriend - look me up on the plexapp.com forums for help
+#Written by mysciencefriend
+#Overhauled and updated by Mikedm139
 #Use at your own risk, etc. etc.
 
 VIDEO_PREFIX = '/video/cbc'
@@ -12,72 +10,213 @@ NAME = 'CBC'
 ART  = 'art-default.jpg'
 ICON = 'icon-default.png'
 
-CATEGORY_LIST = {'News'		:	'1221258968',
-				'Sports'	:	'1237510081',
-				'BC'		:	'1317899897',
-				'Calgary'	:	'1317901071',
-				'Edmonton'	:	'1317901477',
-				'Manitoba'	:	'1317902896',
-				'Montreal'	:	'1317903731',
-				'NB'		:	'1317906492',
-				'NL'		:	'1317907201',
-				'North'		:	'1317907460',
-				'NS'		:	'1317909223',
-				'Ottawa'	:	'1317910092',
-				'PEI'		:	'1317910853',
-				'Saskatchewan'	:	'1317911675',
-				'Toronto'	:	'1317912017',
-				'Windsor'	:	'1317913355'
-				}
+SHOWS = '2248506087'
+MORE_SHOWS = '1329813962'
 
-SHOWEXCEPTIONS = {'The Rick Mercer Report'				:	'Rick Mercer Report',
-				'The Ron James Show'			:	'Ron James Show',
-				'Hockey Night in Canada'		:	'HNIC',
-				'Q on bold'				:	'Q',
-				'George Stroumboulopoulos Tonight'	:	'George Tonight'
-				}
+#NEWS_LIST = {'News'		:	'1221258968',
+#				'Sports'	:	'1237510081',
+#				'BC'		:	'1317899897',
+#				'Calgary'	:	'1317901071',
+#				'Edmonton'	:	'1317901477',
+#				'Manitoba'	:	'1317902896',
+#				'Montreal'	:	'1317903731',
+#				'NB'		:	'1317906492',
+#				'NL'		:	'1317907201',
+#				'North'		:	'1317907460',
+#				'NS'		:	'1317909223',
+#				'Ottawa'	:	'1317910092',
+#				'PEI'		:	'1317910853',
+#				'Saskatchewan'	:	'1317911675',
+#				'Toronto'	:	'1317912017',
+#				'Windsor'	:	'1317913355'
+#				}
 
-SHOWEXCEPTIONS_REV = {'Rick Mercer Report'			:	'The Rick Mercer Report',
-					'Ron James Show'	:	'The Ron James Show',
-					'HNIC'			:	'Hockey Night in Canada',
-					'Q'			:	'Q on bold',
-					'George Tonight'	:	'George Stroumboulopoulos Tonight'
-					}
+#SHOWEXCEPTIONS = {'The Rick Mercer Report'				:	'Rick Mercer Report',
+#				'The Ron James Show'			:	'Ron James Show',
+#				'Hockey Night in Canada'		:	'HNIC',
+#				'Q on bold'				:	'Q',
+#				'George Stroumboulopoulos Tonight'	:	'George Tonight'
+#				}
 
-SHOWSLIST = 'http://cbc.feeds.theplatform.com/ps/JSON/PortalService/2.2/getCategoryList?PID=_DyE_l_gC9yXF9BvDQ4XNfcCVLS4PQij&field=ID&field=title&field=parentID&field=description&customField=MaxClips&customField=ClipType&query=ParentIDs|1221254309'
-LOCALLIST = 'http://cbc.feeds.theplatform.com/ps/JSON/PortalService/2.2/getCategoryList?PID=_DyE_l_gC9yXF9BvDQ4XNfcCVLS4PQij&field=ID&field=title&field=parentID&field=description&customField=MaxClips&customField=ClipType&query=ParentIDs|1244502941'
-CLIPSURL = 'http://cbc.feeds.theplatform.com/ps/JSON/PortalService/2.2/getReleaseList?PID=_DyE_l_gC9yXF9BvDQ4XNfcCVLS4PQij&query=CategoryIDs|%s&sortField=%s&sortDescending=true&endIndex=500'
-SHOWSURL = 'http://cbc.feeds.theplatform.com/ps/JSON/PortalService/2.2/getReleaseList?PID=_DyE_l_gC9yXF9BvDQ4XNfcCVLS4PQij&field=title&field=PID&field=ID&field=description&field=categoryIDs&field=thumbnailURL&field=URL&field=added&field=airdate&field=expirationDate&field=length&field=Keywords&query=%s&sortField=airdate&sortDescending=true&startIndex=1&endIndex=%s'
-WATCHURL = 'http://www.cbc.ca/video/#/%s/ID='
+#SHOWEXCEPTIONS_REV = {'Rick Mercer Report'			:	'The Rick Mercer Report',
+#					'Ron James Show'	:	'The Ron James Show',
+#					'HNIC'			:	'Hockey Night in Canada',
+#					'Q'			:	'Q on bold',
+#					'George Tonight'	:	'George Stroumboulopoulos Tonight'
+#					}
 
-SMIL_URL = 'http://link.theplatform.com/s/h9dtGB/zRoOKQ_cN9OQOikWISihO5bmV8zWB3Xs?isPLS=false&airdate=1301356800000&site=cbcentca&zone=little_mosque_on_the_prairie&shortClip=false&show=little_mosque_on_the_prairie&liveondemand=ondemand&type=full_program&season=5&format=SMIL&Tracking=true&Embedded=true'
+SHOWS_LIST = 'http://cbc.feeds.theplatform.com/ps/JSON/PortalService/2.2/getCategoryList?PID=_DyE_l_gC9yXF9BvDQ4XNfcCVLS4PQij&field=ID&field=title&field=parentID&field=description&customField=MaxClips&customField=ClipType&query=ParentIDs|%s'
+VIDEOS_LIST = 'http://cbc.feeds.theplatform.com/ps/JSON/PortalService/2.2/getReleaseList?PID=_DyE_l_gC9yXF9BvDQ4XNfcCVLS4PQij&query=CategoryIDs|%s&sortDescending=true&endIndex=500'
+#LOCALLIST = 'http://cbc.feeds.theplatform.com/ps/JSON/PortalService/2.2/getCategoryList?PID=_DyE_l_gC9yXF9BvDQ4XNfcCVLS4PQij&field=ID&field=title&field=parentID&field=description&customField=MaxClips&customField=ClipType&query=ParentIDs|1244502941'
+#SHOWSURL = 'http://cbc.feeds.theplatform.com/ps/JSON/PortalService/2.2/getReleaseList?PID=_DyE_l_gC9yXF9BvDQ4XNfcCVLS4PQij&field=title&field=PID&field=ID&field=description&field=categoryIDs&field=thumbnailURL&field=URL&field=added&field=airdate&field=expirationDate&field=length&field=Keywords&query=%s&sortField=airdate&sortDescending=true&startIndex=1&endIndex=%s'
+BASE_URL = 'http://www.cbc.ca'
+PLAYER_URL = BASE_URL + '/player/%s/' 
+
+CATEGORIES = ['News', 'Sports', 'Digital Archives']
+
+#SMIL_URL = 'http://link.theplatform.com/s/h9dtGB/zRoOKQ_cN9OQOikWISihO5bmV8zWB3Xs?isPLS=false&airdate=1301356800000&site=cbcentca&zone=little_mosque_on_the_prairie&shortClip=false&show=little_mosque_on_the_prairie&liveondemand=ondemand&type=full_program&season=5&format=SMIL&Tracking=true&Embedded=true'
 
 ####################################################################################################
-
 def Start():
     Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, NAME, ICON, ART)
-    Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
 
-    MediaContainer.art = R(ART)
-    MediaContainer.title1 = NAME
-    MediaContainer.viewGroup = 'InfoList'
-    DirectoryItem.thumb = R(ICON)
-
-    HTTP.CacheTime = 1800
+    ObjectContainer.art = R(ART)
+    ObjectContainer.title1 = NAME
+    DirectoryObject.thumb = R(ICON)
 
 ####################################################################################################
-
 def VideoMainMenu():
-	dir = MediaContainer()
-
-	dir.Append(Function(DirectoryItem(NewsMenu,L('NewsMenu'),subtitle="",summary=L('NewsSummary'),thumb=R(ICON),art=R(ART))))
-	dir.Append(Function(DirectoryItem(ShowsMenu,L('ShowsMenu'),subtitle="",summary=L('ShowsSummary'),thumb=R(ICON),art=R(ART),),level=1, title2=L('ShowsMenu')))
-	dir.Append(Function(DirectoryItem(SportsMenu,L('SportsMenu'),subtitle="",summary=L('SportsSummary'),thumb=R(ICON),art=R(ART))))
-
-	return dir
+    oc = ObjectContainer()
+    
+    oc.add(DirectoryObject(key=Callback(ShowCategories), title=L('SHOWSMENU')))
+    for category in CATEGORIES:
+	oc.add(DirectoryObject(key=Callback(NewsCategories, category=category), title=category))
+    
+    return oc
 
 ####################################################################################################
+def ShowCategories():
+    oc = ObjectContainer(title2=L('SHOWSMENU'))
+    data = HTML.ElementFromURL(PLAYER_URL % 'Shows')
+    categories = data.xpath('//div[@id="catnav"]//a[@class="haschildren"]')
+    for category in categories:
+	title = category.text
+	cat_id = category.xpath('parent::li')[0].get('data-id')
+	oc.add(DirectoryObject(key=Callback(ShowsMenu, id=cat_id, title2=title), title=title))
+    return oc
 
+####################################################################################################
+def ShowsMenu(id, title1=None, title2=None):
+    oc = ObjectContainer()
+    if title1:
+	oc.title1=title1
+    if title2:
+	oc.title2=title2
+    
+    data = JSON.ObjectFromURL(SHOWS_LIST % id)
+    
+    if data['listInfo']['itemCount'] == 0:
+	return VideoMenu(id, title1, title2)
+    else:
+	pass
+    
+    for item in data['items']:
+	title = item['title']
+	Log(title)
+	id = item['ID']
+	Log(id)
+	oc.add(DirectoryObject(key=Callback(ShowsMenu, id=id, title1=title2, title2=title), title=title))
+	
+    oc.objects.sort(key = lambda obj: obj.title)
+    if title2 == 'Shows':
+	oc.add(DirectoryObject(key=Callback(ShowsMenu, id=MORE_SHOWS, title2='More Shows'), title='More Shows'))
+	
+    return oc
+
+####################################################################################################
+def NewsCategories(category):
+    oc = ObjectContainer(title2=category)
+    data = HTML.ElementFromURL(PLAYER_URL % String.Quote(category, usePlus=True))
+    for item in data.xpath('//div[@id="catnav"]//a'):
+	url = item.get('href')
+	name = item.text
+	oc.add(DirectoryObject(key=Callback(NewsSortMenu, title1=category, title2=name, url=url), title=name))
+    
+    if len(oc) == 0:
+	return ObjectContainer(header=L("Empty"), message=L('No content found.'))
+    else:
+	return oc
+
+####################################################################################################
+def NewsSortMenu(title1, title2, url):
+    oc = ObjectContainer(title1=title1, title2=title2)
+    for option in ['Featured', 'Most Popular', 'Most Recent']:
+	oc.add(DirectoryObject(key=Callback(NewsMenu, title1=title1, title2=title2, url=url,
+	    sort_type=option.replace(' ', '')), title=option))
+    return oc
+
+####################################################################################################
+def NewsMenu(title1, title2, url, sort_type=None, page=1, categories=[]):
+    oc = ObjectContainer(title1=title1, title2=title2)
+    data_url = BASE_URL + url + '?page=' + str(page)
+    if sort_type:
+	data_url = data_url + '&sort=' + sort_type
+    data = HTML.ElementFromURL(data_url)
+    if page == 1:
+	subcategories = data.xpath('//div[@id="catnav"]//a')
+	new_categories = [subcategory.text for subcategory in subcategories]
+	for subcategory in subcategories:
+	    url = subcategory.get('href')
+	    name = subcategory.text
+	    if name not in categories:
+		oc.add(DirectoryObject(key=Callback(NewsMenu, title1=title2, title2=name, url=url, sort_type=sort_type, page=page, categories=new_categories), title=name))
+	    else:
+		pass
+    else:
+	pass
+    
+    for clip in data.xpath('//div[@class="clips"]//div[contains(@class, "clip col")]'):
+	clip_url = BASE_URL + clip.xpath('.//a')[0].get('href')
+	title = clip.xpath('.//img')[0].get('alt')
+	thumb = clip.xpath('.//img')[0].get('src')
+	try:
+	    date = Datetime.ParseDate(clip.xpath('.//span[@class="date"]')[0].text).date()
+	except:
+	    date=None
+	try:
+	    runtime = clip.xpath('.//span[@class="length"]')[0].text.split(':')
+	    if len(runtime) == 3:
+		duration = ((int(runtime[0])*60 + int(runtime[1]))*60 + int(runtime[2]))*1000
+	    elif len(runtime) == 2:
+	        duration = (int(runtime.split(':')[0])*60 + int(runtime.split(':')[1]))*1000
+	    else:
+	        duration = int(runtime[0])*1000
+	except:
+	    duration = None
+	summary = clip.xpath('.//span[@class="desc"]')[0].text
+	oc.add(VideoClipObject(url=clip_url, title=title, summary=summary, duration=duration, originally_available_at=date,
+	    thumb=Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)))
+    
+    try:
+	total_pages = int(data.xpath('//span[@class="totalpages"]')[0].text)
+    except:
+	total_pages = 1
+    if total_pages > page:
+	page = page+1
+	oc.add(DirectoryObject(key=Callback(NewsMenu, title1=title2, title2="Page %s" % page, url=url, sort_type=sort_type, page=page), title='More'))
+    
+    if len(oc) == 0:
+	return ObjectContainer(header=L("Empty"), message=L('No content found.'))
+    else:
+	return oc
+    
+####################################################################################################
+def VideoMenu(id, title1=None, title2=None):
+    oc = ObjectContainer()
+    if title1:
+	oc.title1=title1
+    if title2:
+	oc.title2=title2
+    data = JSON.ObjectFromURL(VIDEOS_LIST % id)
+    for item in data['items']:
+	title = item['title']
+	summary = item['description']
+	duration = int(item['length'])
+	date = int(item['airdate'])/1000
+	Log(date)
+	date = Datetime.FromTimestamp(date).date()
+	thumbs = sorted(item['assets'], key = lambda thumb: int(thumb["height"]), reverse=True)
+	id = item['ID']
+	oc.add(VideoClipObject(url=PLAYER_URL % id, title=title, summary=summary, originally_available_at=date,
+	    thumb=Resource.ContentsOfURLWithFallback(url=[thumb['URL'] for thumb in thumbs], fallback=ICON)))
+    
+    if len(oc) == 0:
+	return ObjectContainer(header=L("Empty"), message=L('No content found.'))
+    else:
+	return oc
+
+####################################################################################################
+'''
 def NewsMenu(sender):
 	url = 'http://www.cbc.ca/thenational/watch/'
 	dir = MediaContainer(title2="News")
@@ -251,3 +390,4 @@ def LocalMenu(sender, title2, showName="",category="", currentPage=0, clipType="
 		showName = String.Quote(show['title'], usePlus=False)
 		dir.Append(Function(DirectoryItem(GetClips,title=title,subtitle="",summary=L('NewsLocalSummary'),thumb=R(ICON),art=R(ART),),category=title, arrange="airdate", title2=L('Latest')))
 	return dir
+'''
